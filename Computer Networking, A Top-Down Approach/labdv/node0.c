35b1995c7cf226ab3aa1382fe1d3841a245af6f5
cuongd <cuongd@pm.me>
2021-09-14 23:16:21 +0700

    natda:labdv: Complete node0.c

--- Computer Networking, A Top-Down Approach/labdv/node0.c
+++ Computer Networking, A Top-Down Approach/labdv/node0.c
@@ -18,23 +18,76 @@ struct distance_table
 
 
 /* students to write the following two routines, and maybe some others */
+extern float clocktime;
+void printdt0(struct distance_table *dt0);
+void tolayer2(struct rtpkt);
+void broadcast();
 
 void rtinit0()
 {
+  int i, j;
+  struct rtpkt rtpkt;
 
+  /* costs: 0: itself, -1: unknown, 999: unreachable */
+  rtpkt.mincost[0] = dt0.costs[0][0] = 0;
+  rtpkt.mincost[1] = dt0.costs[0][1] = 1;
+  rtpkt.mincost[2] = dt0.costs[0][2] = 3;
+  rtpkt.mincost[3] = dt0.costs[0][3] = 7;
+  for (i = 1; i < 4; i++)
+    for (j = 0; j < 4; j++)
+      if (i == j) dt0.costs[i][j] = 0;
+      else dt0.costs[i][j] = -1;
+  printf("time %.0f: node0 initialization\n", clocktime);
+  printdt0(&dt0);
+  broadcast();
 }
-
 
 void rtupdate0(rcvdpkt)
   struct rtpkt *rcvdpkt;
 {
+  int dtupdated = 0;
+  int new_cost_via;
+  int i;
 
+  printf("time %.0f: from %d to %d\n",
+         clocktime, rcvdpkt->sourceid, rcvdpkt->destid);
+  for (i = 0; i < 4; i++) {
+    if (dt0.costs[rcvdpkt->sourceid][i] == rcvdpkt->mincost[i]) {
+      continue;
+    } else {
+      printf("  new mindv update: %d->%d: %d->%d\n", rcvdpkt->sourceid, i,
+             dt0.costs[rcvdpkt->sourceid][i], rcvdpkt->mincost[i]);
+      dt0.costs[rcvdpkt->sourceid][i] = rcvdpkt->mincost[i];
+      new_cost_via = dt0.costs[0][rcvdpkt->sourceid] + rcvdpkt->mincost[i];
+      if (dt0.costs[0][i] > new_cost_via) {
+        printf("  new mindv calc: 0->%d: %d->%d\n",
+               i, dt0.costs[0][i], new_cost_via);
+        if (!dtupdated) dtupdated = 1;
+        dt0.costs[0][i] = new_cost_via;
+      }
+    }
+  }
+  if (dtupdated) broadcast();
 }
 
+void broadcast()
+{
+  struct rtpkt rtpkt;
+  int i;
 
-printdt0(dtptr)
+  rtpkt.sourceid = 0;
+  rtpkt.mincost[0] = dt0.costs[0][0];
+  rtpkt.mincost[1] = dt0.costs[0][1];
+  rtpkt.mincost[2] = dt0.costs[0][2];
+  rtpkt.mincost[3] = dt0.costs[0][3];
+  for (i = 1; i < 4; i++) {
+    rtpkt.destid = i;
+    tolayer2(rtpkt);
+  }
+}
+
+void printdt0(dtptr)
   struct distance_table *dtptr;
-
 {
   printf("                via     \n");
   printf("   D0 |    1     2    3 \n");
