68ab4e94e83f2aa483b4defe51c9c91b75153a04
cuongd <cuongd@pm.me>
2021-09-15 10:51:59 +0700

    algsp1:ex1.3.20:adt:list: Add delete method

--- Algorithms, Part I/src/main/java/com/cuongd/study/algsp1/book/ch1/MyLinkedList.java
+++ Algorithms, Part I/src/main/java/com/cuongd/study/algsp1/book/ch1/MyLinkedList.java
@@ -2,9 +2,14 @@ package com.cuongd.study.algsp1.book.ch1;
 
 public class MyLinkedList<Item> {
     public Node first;
+    private int N;
 
     public boolean isEmpty() {
-        return first == null;
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
     }
 
     public void add(Item item) {
@@ -12,6 +17,24 @@ public class MyLinkedList<Item> {
         first = new Node();
         first.value = item;
         first.next = rest;
+        N++;
+    }
+
+    /** Delete the k-th element. Assume it exists. */
+    public void delete(int k) {
+        N--;
+        if (k == 0) {
+            first = first.next;
+            return;
+        }
+
+        Node prev = first, curr = first;
+        while (k > 0) {
+            k--;
+            prev = curr;
+            curr = curr.next;
+        }
+        prev.next = curr.next;
     }
 
     class Node {
--- Algorithms, Part I/src/test/java/com/cuongd/study/algsp1/book/ch1/MyLinkedListTest.java
+++ Algorithms, Part I/src/test/java/com/cuongd/study/algsp1/book/ch1/MyLinkedListTest.java
@@ -16,16 +16,46 @@ public class MyLinkedListTest {
     @Test
     public void testConstructor() {
         assertTrue(list.isEmpty());
+        assertEquals(0, list.size());
     }
 
     @Test
     public void testAdd() {
         list.add(1);
         assertFalse(list.isEmpty());
+        assertEquals(1, list.size());
         assertEquals(1, (int) list.first.value);
         list.add(2);
+        assertEquals(2, list.size());
         assertEquals(2, (int) list.first.value);
         assertEquals(1, (int) list.first.next.value);
         assertNull(list.first.next.next);
+    }
+
+    @Test
+    public void testDelete0() {
+        list.add(1); list.add(2); list.add(3); list.add(4);
+        list.delete(0);
+        assertEquals(3, list.size());
+        assertEquals(3, (int) list.first.value);
+    }
+
+    @Test
+    public void testDelete1() {
+        list.add(1); list.add(2); list.add(3); list.add(4);
+        list.delete(1);
+        assertEquals(3, list.size());
+        assertEquals(4, (int) list.first.value);
+        assertEquals(2, (int) list.first.next.value);
+    }
+
+    @Test
+    public void testDelete3() {
+        list.add(1); list.add(2); list.add(3); list.add(4);
+        list.delete(3);
+        assertEquals(3, list.size());
+        assertEquals(4, (int) list.first.value);
+        assertEquals(3, (int) list.first.next.value);
+        assertEquals(2, (int) list.first.next.next.value);
     }
 }
