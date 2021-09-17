e035426fb3ac87f5f649e09f646421f5084a93ca
cuongd <cuongd@pm.me>
2021-09-17 22:05:20 +0700

    csapp:labcache: Complete part B's first matrix
    I decided to move on with this lab without finishing the last two
    matrices. I've got a good understanding of how cache works. However, to
    complete these kinds of assignments demands much of a "hacky" mindset
    and ingenuity that I lack. I spent a good number of hours on this lab
    but hit a big wall trying to get the full score with the first
    matrix. I needed to get some hints from other students to get the idea
    of using temp variables to reduce conflict misses. The solution is not
    something I find beautiful and elegant so I do not find much
    satisfaction solving these kinds of problems.

--- Computer Systems, A Programmer's Perspective/labcache/trans.c
+++ Computer Systems, A Programmer's Perspective/labcache/trans.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -6,30 +6,91 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-/* 
+/*
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
  *     the description string "Transpose submission", as the driver
  *     searches for that string to identify the transpose function to
- *     be graded. 
+ *     be graded.
  */
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+#define BLOCK_SIZE 8
+    int i, j, ii, jj;
+    /* Temp variables to reduce conflict misses. */
+    int t0, t1, t2, t3, t4, t5, t6, t7;
+
+    for (i = 0; i < N-BLOCK_SIZE+1; i += BLOCK_SIZE) {
+        for (j = 0; j < M-BLOCK_SIZE+1; j += BLOCK_SIZE) {
+            for (ii = i; ii < i+BLOCK_SIZE; ii++) {
+                for (jj = j; jj < j+BLOCK_SIZE; jj += BLOCK_SIZE) {
+                    t0 = A[ii][jj+0];
+                    t1 = A[ii][jj+1];
+                    t2 = A[ii][jj+2];
+                    t3 = A[ii][jj+3];
+                    t4 = A[ii][jj+4];
+                    t5 = A[ii][jj+5];
+                    t6 = A[ii][jj+6];
+                    t7 = A[ii][jj+7];
+
+                    B[jj+0][ii] = t0;
+                    B[jj+1][ii] = t1;
+                    B[jj+2][ii] = t2;
+                    B[jj+3][ii] = t3;
+                    B[jj+4][ii] = t4;
+                    B[jj+5][ii] = t5;
+                    B[jj+6][ii] = t6;
+                    B[jj+7][ii] = t7;
+                }
+            }
+        }
+        /* Finish off rest elements for "odd" matrix sizes. */
+        if (j == M) continue;
+        for (j = j-BLOCK_SIZE+1; j < M; j++) {
+            for (ii = i; ii < i+BLOCK_SIZE; ii++) {
+                for (jj = j; jj < j+BLOCK_SIZE; jj += BLOCK_SIZE) {
+                    t0 = A[ii][jj+0];
+                    t1 = A[ii][jj+1];
+                    t2 = A[ii][jj+2];
+                    t3 = A[ii][jj+3];
+                    t4 = A[ii][jj+4];
+                    t5 = A[ii][jj+5];
+                    t6 = A[ii][jj+6];
+                    t7 = A[ii][jj+7];
+
+                    B[jj+0][ii] = t0;
+                    B[jj+1][ii] = t1;
+                    B[jj+2][ii] = t2;
+                    B[jj+3][ii] = t3;
+                    B[jj+4][ii] = t4;
+                    B[jj+5][ii] = t5;
+                    B[jj+6][ii] = t6;
+                    B[jj+7][ii] = t7;
+                }
+            }
+        }
+    }
+    if (i == N) return;
+    for (i = i-BLOCK_SIZE+1; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            B[j][i] = A[i][j];
+        }
+    }
 }
 
-/* 
+/*
  * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
+ * a simple one below to help you get started.
+ */
 
-/* 
+/*
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
 char trans_desc[] = "Simple row-wise scan transpose";
@@ -42,8 +103,7 @@ void trans(int M, int N, int A[N][M], int B[M][N])
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
-
+    }
 }
 
 /*
@@ -56,14 +116,10 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
-
-    /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 }
 
-/* 
+/*
  * is_transpose - This helper function checks if B is the transpose of
  *     A. You can check the correctness of your transpose by calling
  *     it before returning from the transpose function.
@@ -81,4 +137,3 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
     }
     return 1;
 }
-
