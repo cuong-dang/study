ec556c8f0adabf0a3960bb04f2537db687e0d322
cuongd <cuongd@pm.me>
2021-09-28 23:38:23 +0700

    fpdis/wk5: Complete Caculator

--- Functional Program Design in Scala/wk5/calculator/src/main/scala/calculator/Calculator.scala
+++ Functional Program Design in Scala/wk5/calculator/src/main/scala/calculator/Calculator.scala
@@ -11,11 +11,41 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator extends CalculatorInterface {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+    namedExpressions.map {
+      case (ref, sigExpr) =>
+        if (isCyclicRef(namedExpressions(ref)(), namedExpressions, Set(ref)))
+          (ref, Signal(Double.NaN))
+        else (ref, Signal(eval(sigExpr(), namedExpressions)))
+    }
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    expr match {
+      case Literal(v) => v
+      case Plus(a, b) => eval(a, references) + eval(b, references)
+      case Minus(a, b) => eval(a, references) - eval(b, references)
+      case Times(a, b) => eval(a, references) * eval(b, references)
+      case Divide(a, b) => eval(a, references) / eval(b, references)
+      case Ref(name) => eval(getReferenceExpr(name, references), references)
+    }
+  }
+
+  private def isCyclicRef(expr: Expr, references: Map[String, Signal[Expr]],
+                          cyclicRefs: Set[String]): Boolean = {
+    expr match {
+      case Literal(_) => false
+      case Plus(a, b) => isCyclicRef(a, references, cyclicRefs) ||
+        isCyclicRef(b, references, cyclicRefs)
+      case Minus(a, b) => isCyclicRef(a, references, cyclicRefs) ||
+        isCyclicRef(b, references, cyclicRefs)
+      case Times(a, b) => isCyclicRef(a, references, cyclicRefs) ||
+        isCyclicRef(b, references, cyclicRefs)
+      case Divide(a, b) => isCyclicRef(a, references, cyclicRefs) ||
+        isCyclicRef(b, references, cyclicRefs)
+      case Ref(name) =>
+        if (cyclicRefs.contains(name)) true
+        else isCyclicRef(getReferenceExpr(name, references), references, cyclicRefs + name)
+    }
   }
 
   /** Get the Expr for a referenced variables.
