0564d7094f0cbfa27ab29182c5bae2b356558e5f
cuongd <cuongd@pm.me>
2021-10-01 23:10:35 +0700

    lcpis/ch1: Add exercise 1.5

---
+++ Learning Concurrent Programming in Scala/build.sbt
@@ -0,0 +1,9 @@
+name := "Learning Concurrent Programming in Scala"
+
+version := "0.1"
+
+scalaVersion := "2.12.15"
+
+idePackagePrefix := Some("com.cuongd.study.lcpis")
+
+libraryDependencies += "org.scalatest" %% "scalatest-flatspec" % "3.2.9" % Test
---
+++ Learning Concurrent Programming in Scala/src/test/scala/Ex15Spec.scala
@@ -0,0 +1,27 @@
+package com.cuongd.study.lcpis
+
+import Ex15.permutations
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class Ex15Spec extends AnyFlatSpec {
+  "permutations" should "return permutations of empty string" in {
+    assertResult(Seq(""))(permutations(""))
+  }
+
+  it should "return permutations of one-length string" in {
+    assertResult(Seq("a"))(permutations("a"))
+  }
+
+  it should "return permutations of two-length string" in {
+    assertResult(Seq("ab", "ba"))(permutations("ab"))
+  }
+
+  it should "return permutations of longer strings" in {
+    assertResult(Seq("abc", "acb", "bac", "bca", "cab", "cba"))(permutations("abc"))
+  }
+
+  it should "return permutations of longer strings with duplicates" in {
+    assertResult(Seq("aac", "aca", "caa"))(permutations("aac"))
+  }
+}
