aa94301418dec1a973ad5b6db1bfdc46aea3bf8d
cuongd <cuongd@pm.me>
2021-10-04 08:39:31 +0700

    algsp1/ch1: Add exercise 1.4.27

---
+++ Algorithms, Part I/src/main/java/com/cuongd/study/algsp1/book/ch1/Ex1427.java
@@ -0,0 +1,30 @@
+package com.cuongd.study.algsp1.book.ch1;
+
+/** Queue with two stacks */
+public class Ex1427<Item> {
+    private final MyStack<Item> enq;
+    private final MyStack<Item> deq;
+
+    public Ex1427() {
+        enq = new MyStack<>();
+        deq = new MyStack<>();
+    }
+
+    public boolean isEmpty() {
+        return enq.isEmpty() && deq.isEmpty();
+    }
+
+    public void enqueue(Item item) {
+        enq.push(item);
+    }
+
+    public Item dequeue() {
+        if (deq.isEmpty()) {
+            while (!enq.isEmpty()) {
+                Item item = enq.pop();
+                deq.push(item);
+            }
+        }
+        return deq.pop();
+    }
+}
---
+++ Algorithms, Part I/src/test/java/com/cuongd/study/algsp1/book/ch1/Ex1427Test.java
@@ -0,0 +1,44 @@
+package com.cuongd.study.algsp1.book.ch1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Ex1427Test {
+    private Ex1427<Integer> q;
+
+    @Before
+    public void setup() {
+        q = new Ex1427<>();
+    }
+
+    @Test
+    public void testEnqueue() {
+        assertTrue(q.isEmpty());
+        q.enqueue(1);
+        assertFalse(q.isEmpty());
+    }
+
+    @Test
+    public void testMixedOps() {
+        q.enqueue(1);
+        assertEquals(1, (int) q.dequeue());
+        q.enqueue(1);
+        q.enqueue(2);
+        assertEquals(1, (int) q.dequeue());
+        assertEquals(2, (int) q.dequeue());
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        assertEquals(1, (int) q.dequeue());
+        q.enqueue(4);
+        assertEquals(2, (int) q.dequeue());
+        assertEquals(3, (int) q.dequeue());
+        q.enqueue(5);
+        q.enqueue(6);
+        assertEquals(4, (int) q.dequeue());
+        assertEquals(5, (int) q.dequeue());
+        assertEquals(6, (int) q.dequeue());
+    }
+}
