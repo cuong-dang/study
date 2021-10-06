640a9e4debd77860e608a9179ea587ea375c645e
cuongd <cuongd@pm.me>
2021-10-06 16:18:04 +0700

    algsp1/ch1: Add exercise 1.5.22

---
+++ Algorithms, Part I/src/main/java/com/cuongd/study/algsp1/book/ch1/Ex1522.java
@@ -0,0 +1,19 @@
+package com.cuongd.study.algsp1.book.ch1;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class Ex1522 {
+    private static final int T = 32;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < T; i++) {
+            int N = (int) Math.pow(2, i), count;
+
+            Stopwatch timer = new Stopwatch();
+            count = Ex1517.count(N);
+            StdOut.printf("N=%d\tcount=%.2f\ttime=%.4f\n",
+                    N, (double) count/N, timer.elapsedTime());
+        }
+    }
+}
