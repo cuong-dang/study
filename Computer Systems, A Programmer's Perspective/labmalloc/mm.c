8858edc71288fffdd4fa7d98e6f0760645c5efbd
cuongd <cuongd@pm.me>
2021-10-16 14:43:42 +0700

    csapp/labmalloc: Fix mm_alloc to be functional

--- Computer Systems, A Programmer's Perspective/labmalloc/mm.c
+++ Computer Systems, A Programmer's Perspective/labmalloc/mm.c
@@ -34,7 +34,7 @@ typedef unsigned char byte;
 static wordptr prologue;
 static wordptr wilderness;
 
-#define SBRK_SIZE (1 << 31)
+#define SBRK_SIZE (1 << 12)
 
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
@@ -81,12 +81,13 @@ static wordptr wilderness;
 #define FTR(p) ((wordptr)((byte *)SUCC_BLK(p) - WORD_SIZE))
 #define NEXT_BLK(p) (*(wordptr *)(p + 2))
 #define PREV_BLK(p) (*(wordptr *)(p + 1))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 /* function prototypes */
 static inline size_t get_freelist_size(int freelist_i);
 static inline int find_freelist(size_t size);
 static wordptr find_best_fit(wordptr first_block, size_t size);
-static wordptr extend_freelist(int freelist_i);
+static wordptr extend_freelist(int freelist_i, size_t size);
 
 /*
  * mm_init - Initialize the malloc package.
@@ -113,21 +114,23 @@ int mm_init(void)
 void *mm_malloc(size_t size)
 {
     int freelist_i;
+    size_t freelist_sz;
     wordptr p;
 
     if (size == 0)
         return NULL;
     freelist_i = find_freelist(size);
-    if ((p = find_best_fit(FL(freelist_i), size)) == NULL ||
-            (p = extend_freelist(freelist_i)) == NULL)
+    freelist_sz = get_freelist_size(freelist_i);
+    if ((p = find_best_fit(FL(freelist_i), size)) == NULL &&
+            (p = extend_freelist(freelist_i, MAX(freelist_sz, size))) == NULL)
         return NULL;
     *p |= ALLOCATED;
     *(SUCC_BLK(p)) |= PREV_ALLOCATED;
-    if (p + 1 != NULL)
+    if (*(wordptr *)(p + 1) != NULL)
         *(wordptr *)(PREV_BLK(p) + 2) = p + 2;
     else /* first block of list */
         *(wordptr *)FL(freelist_i) = NEXT_BLK(p);
-    if (p + 2 != NULL)
+    if (*(wordptr *)(p + 2) != NULL)
         *(wordptr *)(NEXT_BLK(p) + 1) = PREV_BLK(p);
     /* TODO split block */
     return PAYLOAD(p);
@@ -173,13 +176,13 @@ static inline size_t get_freelist_size(int freelist_i)
  */
 static inline int find_freelist(size_t size)
 {
-    if (size <= FL1_MAXSIZE) return 1;
-    if (size <= FL2_MAXSIZE) return 2;
-    if (size <= FL3_MAXSIZE) return 3;
-    if (size <= FL4_MAXSIZE) return 4;
-    if (size <= FL5_MAXSIZE) return 5;
-    if (size <= FL6_MAXSIZE) return 6;
-    return 7;
+    if (size <= FL1_MAXSIZE) return 0;
+    if (size <= FL2_MAXSIZE) return 1;
+    if (size <= FL3_MAXSIZE) return 2;
+    if (size <= FL4_MAXSIZE) return 3;
+    if (size <= FL5_MAXSIZE) return 4;
+    if (size <= FL6_MAXSIZE) return 5;
+    return 6;
 }
 
 static wordptr find_best_fit(wordptr first_block, size_t size)
@@ -187,6 +190,8 @@ static wordptr find_best_fit(wordptr first_block, size_t size)
     wordptr best_block = first_block, next_block = first_block;
     size_t best_fit_size = BLK_SIZE(best_block), next_block_size;
 
+    if (*first_block == NULL)
+        return NULL;
     while ((next_block = NEXT_BLK(next_block)) != NULL) {
         next_block_size = BLK_SIZE(next_block);
         if (next_block_size >= size && next_block_size < best_fit_size) {
@@ -197,21 +202,21 @@ static wordptr find_best_fit(wordptr first_block, size_t size)
     return next_block;
 }
 
-static wordptr extend_freelist(int freelist_i)
+static wordptr extend_freelist(int freelist_i, size_t size)
 {
-    size_t extend_size = get_freelist_size(freelist_i);
     wordptr p = wilderness;
 
-    if (((byte *)p + ALIGN(extend_size) > (byte *)mem_heap_hi) ||
+    if (((byte *)p + ALIGN(size) > (byte *)mem_heap_hi) &&
             mem_sbrk(SBRK_SIZE) == (void *)-1)
         return NULL;
-    *p = ALIGN(extend_size); /* set header */
+    *p = ALIGN(size); /* set header */
     *(wordptr *)FTR(p) = p; /* set footer */
     *(wordptr *)(p + 1) = NULL; /* set previous */
-    *(wordptr *)(p + 2) = FL(freelist_i); /* set next */
+    *(wordptr *)(p + 2) = *(wordptr *)FL(freelist_i); /* set next */
     if (*(wordptr *)(p + 2) != NULL)
         *(wordptr *)(NEXT_BLK(p) + 1) = p; /* set next block's previous */
     *(wordptr *)FL(freelist_i) = p; /* set head at prologue */
-    wilderness = (wordptr)((byte *)wilderness + ALIGN(extend_size));
+    *((byte *)p + ALIGN(size)) = ALLOCATED; /* set epilogue */
+    wilderness = (wordptr)((byte *)wilderness + ALIGN(size) + WORD_SIZE);
     return p;
 }
