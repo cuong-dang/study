091cc97e3eb0e40a93ece55c5a8f04d07765ebe4
cuongd <cuongd@pm.me>
2021-10-31 14:02:20 +0700

    algsp1/ch2: Complete exercise 2.2.9

--- Algorithms, Part I/src/main/java/com/cuongd/study/algsp1/book/ch2/MergeSortBottomUp.java
+++ Algorithms, Part I/src/main/java/com/cuongd/study/algsp1/book/ch2/MergeSortBottomUp.java
@@ -6,10 +6,11 @@ public class MergeSortBottomUp extends MergeSortTopDown {
     @Override
     public void sort(Comparable[] a) {
         int n = a.length;
+        Comparable[] aux = new Comparable[n];
         for (int sz = 1; sz < n; sz = 2*sz)
             for (int lo = 0; lo < n - sz; lo += 2*sz) {
                 int mid = lo + sz - 1;
-                merge(a, lo, mid, Math.min(lo + 2*sz - 1, n - 1));
+                merge(a, lo, mid, Math.min(lo + 2*sz - 1, n - 1), aux);
             }
     }
 
--- Algorithms, Part I/src/main/java/com/cuongd/study/algsp1/book/ch2/MergeSortTopDown.java
+++ Algorithms, Part I/src/main/java/com/cuongd/study/algsp1/book/ch2/MergeSortTopDown.java
@@ -3,10 +3,7 @@ package com.cuongd.study.algsp1.book.ch2;
 import java.lang.reflect.InvocationTargetException;
 
 public class MergeSortTopDown extends SortCommon {
-    private static final int MAX_N = 64;
-    private static final Comparable[] aux = new Comparable[MAX_N];
-
-    public static void merge(Comparable[] a, int lo, int mid, int hi) {
+    public static void merge(Comparable[] a, int lo, int mid, int hi, Comparable[] aux) {
         int i = lo, j = mid + 1;
         for (int k = lo; k <= hi; ++k)
             aux[k] = a[k];
@@ -20,15 +17,16 @@ public class MergeSortTopDown extends SortCommon {
 
     @Override
     public void sort(Comparable[] a) {
-        sort(a, 0, a.length - 1);
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1, aux);
     }
 
-    private static void sort(Comparable[] a, int lo, int hi) {
+    private static void sort(Comparable[] a, int lo, int hi, Comparable[] aux) {
         if (lo >= hi) return;
         int mid = lo + (hi - lo) / 2;
-        sort(a, lo, mid);
-        sort(a, mid+1, hi);
-        merge(a, lo, mid, hi);
+        sort(a, lo, mid, aux);
+        sort(a, mid+1, hi, aux);
+        merge(a, lo, mid, hi, aux);
     }
 
     public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException,
