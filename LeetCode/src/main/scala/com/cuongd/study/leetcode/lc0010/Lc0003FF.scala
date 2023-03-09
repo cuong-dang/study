c404e00e43a4a795b8f8835f313ba5feebcb92ff
Cuong Dang <cuongd@pm.me>
2023-03-08 22:41:13 -0600

    lc/0010: Solve 0003FF

---
+++ LeetCode/src/main/scala/com/cuongd/study/leetcode/lc0010/Lc0003FF.scala
@@ -0,0 +1,23 @@
+package com.cuongd.study.leetcode.lc0010
+
+import scala.collection.mutable
+
+object Lc0003FF {
+  def lengthOfLongestSubstring(s: String): Int = {
+    var start = 0
+    var ans = 0
+    val m = mutable.Map[Char, Int]()
+    s.indices.foreach { i =>
+      if (m.contains(s(i))) {
+        start = Math.max(start, m(s(i)))
+      }
+      ans = Math.max(ans, i - start + 1)
+      m(s(i)) = i + 1
+    }
+    ans
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(lengthOfLongestSubstring("abcabcbb"))
+  }
+}
