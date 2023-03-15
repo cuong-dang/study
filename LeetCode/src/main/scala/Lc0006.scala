a47b1989607132880dddab72c9056114800f445f
Cuong Dang <cuongd@pm.me>
2023-03-14 23:21:37 -0500

    lc/0010: Solve 0006

---
+++ LeetCode/src/main/scala/Lc0006.scala
@@ -0,0 +1,35 @@
+import scala.collection.mutable
+import scala.util.control.Breaks.{break, breakable}
+
+object Lc0006 {
+  def convert(s: String, numRows: Int): String = {
+    if (numRows == 1) return s
+    val m = mutable.ArrayBuffer[mutable.ArrayBuffer[Char]]()
+    for (_ <- 0 until numRows) { m += mutable.ArrayBuffer[Char]() }
+    var row = 0
+    var i = 0
+    breakable {
+      while (true) {
+        while (row < numRows) {
+          m(row) += s(i)
+          row += 1
+          i += 1
+          if (i == s.length) break
+        }
+        row -= 1
+        while (row > 1) {
+          row -= 1
+          m(row) += s(i)
+          i += 1
+          if (i == s.length) break
+        }
+        row -= 1
+      }
+    }
+    m.flatten.mkString
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(convert("PAYPALISHIRING", 2))
+  }
+}
