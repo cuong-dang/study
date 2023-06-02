f433fb974086629b585d3bdb736aa7fd4cf1068a
Cuong Dang <cuongd@pm.me>
2023-06-02 15:11:26 -0500

    fpis/ch4: Complete exercise 4.7

--- Functional Programming in Scala/src/main/scala/com/cuongd/study/fpis/ch4/_Either.scala
+++ Functional Programming in Scala/src/main/scala/com/cuongd/study/fpis/ch4/_Either.scala
@@ -14,7 +14,7 @@ sealed trait _Either[+E, +A] {
 
   def flatMap[EE >: E, B](f: A => _Either[EE, B]): _Either[EE, B] =
     this match {
-      case _Left(e) => _Left(e)
+      case _Left(e)  => _Left(e)
       case _Right(a) => f(a)
     }
 
@@ -24,3 +24,19 @@ sealed trait _Either[+E, +A] {
 
 case class _Left[+E](error: E) extends _Either[E, Nothing]
 case class _Right[+A](value: A) extends _Either[Nothing, A]
+
+object _Either {
+  def _Try[A](a: => A): _Either[Exception, A] =
+    try _Right(a)
+    catch { case e: Exception => _Left(e) }
+
+  def sequence[E, A](as: List[_Either[E, A]]): _Either[E, List[A]] =
+    as.foldRight[_Either[E, List[A]]](_Right(Nil))((a, r) => a.map2(r)(_ :: _))
+
+  def traverse[E, A, B](
+      as: List[_Either[E, A]]
+  )(f: A => B): _Either[E, List[B]] =
+    as.foldRight[_Either[E, List[B]]](_Right(Nil))((a, r) =>
+      a.map2(r)(f(_) :: _)
+    )
+}
