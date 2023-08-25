c9caf2203780f3e91ac6313f68c40efe6a2e3092
Cuong Dang <cuongd@pm.me>
2023-08-25 13:48:38 -0500

    xv6/lab1: Complete uptime

---
+++ xv6/lab1/uptime.c
@@ -0,0 +1,12 @@
+#include "user/user.h"
+
+int main() {
+  int t;
+
+  if ((t = uptime()) < 0) {
+    fprintf(2, "uptime: uptime failed\n");
+    exit(1);
+  }
+  printf("uptime: %d ticks\n", t);
+  exit(0);
+}
