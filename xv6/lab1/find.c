291dd3996ab12aff76320f608cd320955c976795
Cuong Dang <cuongd@pm.me>
2023-08-25 13:53:39 -0500

    xv6/lab1: Add regex support for find

--- xv6/lab1/find.c
+++ xv6/lab1/find.c
@@ -1,6 +1,44 @@
-#include "user/user.h"
-#include "kernel/stat.h"
 #include "kernel/fs.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+// Regexp matcher from Kernighan & Pike,
+// The Practice of Programming, Chapter 9.
+
+int matchhere(char *, char *);
+int matchstar(int, char *, char *);
+
+int match(char *re, char *text) {
+  if (re[0] == '^')
+    return matchhere(re + 1, text);
+  do { // must look at empty string
+    if (matchhere(re, text))
+      return 1;
+  } while (*text++ != '\0');
+  return 0;
+}
+
+// matchhere: search for re at beginning of text
+int matchhere(char *re, char *text) {
+  if (re[0] == '\0')
+    return 1;
+  if (re[1] == '*')
+    return matchstar(re[0], re + 2, text);
+  if (re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if (*text != '\0' && (re[0] == '.' || re[0] == *text))
+    return matchhere(re + 1, text + 1);
+  return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, char *re, char *text) {
+  do { // a * matches zero or more instances
+    if (matchhere(re, text))
+      return 1;
+  } while (*text != '\0' && (*text++ == c || c == '.'));
+  return 0;
+}
 
 void find(char *path, char *file_name) {
   int fd;
@@ -39,7 +77,7 @@ void find(char *path, char *file_name) {
         fprintf(2, "find: cannot stat %s\n", buf);
         continue;
       }
-      if (st.type == T_FILE && !strcmp(de.name, file_name)) {
+      if (st.type == T_FILE && match(file_name, de.name)) {
         printf("%s\n", buf);
       } else if (st.type == T_DIR) {
         find(buf, file_name);
