516c8007ac42582ff0dd7ecd9ab81e49188216c6
Cuong Dang <cuongd@pm.me>
2023-11-01 09:26:13 -0500

    libcc: Impl nqueens

---
+++ libcc/src/algs/np.c
@@ -0,0 +1,65 @@
+#include "cc_algs_np.h"
+
+int nqueens_solve(int n, int *board, int i, int *out);
+int nqueens_is_safe(int n, int *board, int i, int j);
+
+int nqueens(int n, int *out) {
+  int board[n][n], i, j;
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      board[i][j] = 0;
+    }
+  }
+  return nqueens_solve(n, (int *)board, 0, out);
+}
+
+int nqueens_solve(int n, int *board, int i, int *out) {
+  int j;
+
+  if (i == n) {
+    return 1;
+  }
+  for (j = 0; j < n; j++) {
+    if (nqueens_is_safe(n, board, i, j)) {
+      out[i] = j;
+      board[i * n + j] = 1;
+      if (nqueens_solve(n, board, i + 1, out)) {
+        return 1;
+      }
+      board[i * n + j] = 0;
+    }
+  }
+  return 0;
+}
+
+int nqueens_is_safe(int n, int *board, int i, int j) {
+  int k, l;
+
+  /* Check columns */
+  for (k = 0; k < i; k++) {
+    if (board[k * n + j]) {
+      return 0;
+    }
+  }
+  /* Check diagonals */
+  k = i - 1;
+  l = j - 1;
+  while (k >= 0 && l >= 0) {
+    if (board[k * n + l]) {
+      return 0;
+    }
+    k--;
+    l--;
+  }
+  k = i - 1;
+  l = j + 1;
+  while (k >= 0 && l < n) {
+    if (board[k * n + l]) {
+      return 0;
+    }
+    k--;
+    l++;
+  }
+  return 1;
+}
---
+++ libcc/src/includes/cc_algs_np.h
@@ -0,0 +1,6 @@
+#ifndef CC_ALGS_NP
+#define CC_ALGS_NP
+
+int nqueens(int n, int *out);
+
+#endif
