86670a67bb2fe8b776c56ea32cacbfbcc563d57c
Cuong Dang <cuongd@pm.me>
2024-01-25 09:32:45 -0600

    xv6/thread: Complete ph

--- xv6/notxv6/ph.c
+++ xv6/notxv6/ph.c
@@ -14,20 +14,17 @@ struct entry {
   struct entry *next;
 };
 struct entry *table[NBUCKET];
+pthread_mutex_t locks[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
-double
-now()
-{
- struct timeval tv;
- gettimeofday(&tv, 0);
- return tv.tv_sec + tv.tv_usec / 1000000.0;
+double now() {
+  struct timeval tv;
+  gettimeofday(&tv, 0);
+  return tv.tv_sec + tv.tv_usec / 1000000.0;
 }
 
-static void 
-insert(int key, int value, struct entry **p, struct entry *n)
-{
+static void insert(int key, int value, struct entry **p, struct entry *n) {
   struct entry *e = malloc(sizeof(struct entry));
   e->key = key;
   e->value = value;
@@ -35,70 +32,65 @@ insert(int key, int value, struct entry **p, struct entry *n)
   *p = e;
 }
 
-static 
-void put(int key, int value)
-{
+static void put(int key, int value) {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&locks[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
   }
-  if(e){
+  if (e) {
     // update the existing key.
     e->value = value;
   } else {
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&locks[i]);
 }
 
-static struct entry*
-get(int key)
-{
+static struct entry *get(int key) {
   int i = key % NBUCKET;
 
-
+  pthread_mutex_lock(&locks[i]);
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
-    if (e->key == key) break;
+    if (e->key == key)
+      break;
   }
+  pthread_mutex_unlock(&locks[i]);
 
   return e;
 }
 
-static void *
-put_thread(void *xa)
-{
-  int n = (int) (long) xa; // thread number
-  int b = NKEYS/nthread;
+static void *put_thread(void *xa) {
+  int n = (int)(long)xa; // thread number
+  int b = NKEYS / nthread;
 
   for (int i = 0; i < b; i++) {
-    put(keys[b*n + i], n);
+    put(keys[b * n + i], n);
   }
 
   return NULL;
 }
 
-static void *
-get_thread(void *xa)
-{
-  int n = (int) (long) xa; // thread number
+static void *get_thread(void *xa) {
+  int n = (int)(long)xa; // thread number
   int missing = 0;
 
   for (int i = 0; i < NKEYS; i++) {
     struct entry *e = get(keys[i]);
-    if (e == 0) missing++;
+    if (e == 0)
+      missing++;
   }
   printf("%d: %d keys missing\n", n, missing);
   return NULL;
 }
 
-int
-main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   pthread_t *tha;
   void *value;
   double t1, t0;
@@ -114,34 +106,37 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
+  for (int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(&locks[i], NULL);
+  }
 
   //
   // first the puts
   //
   t0 = now();
-  for(int i = 0; i < nthread; i++) {
-    assert(pthread_create(&tha[i], NULL, put_thread, (void *) (long) i) == 0);
+  for (int i = 0; i < nthread; i++) {
+    assert(pthread_create(&tha[i], NULL, put_thread, (void *)(long)i) == 0);
   }
-  for(int i = 0; i < nthread; i++) {
+  for (int i = 0; i < nthread; i++) {
     assert(pthread_join(tha[i], &value) == 0);
   }
   t1 = now();
 
-  printf("%d puts, %.3f seconds, %.0f puts/second\n",
-         NKEYS, t1 - t0, NKEYS / (t1 - t0));
+  printf("%d puts, %.3f seconds, %.0f puts/second\n", NKEYS, t1 - t0,
+         NKEYS / (t1 - t0));
 
   //
   // now the gets
   //
   t0 = now();
-  for(int i = 0; i < nthread; i++) {
-    assert(pthread_create(&tha[i], NULL, get_thread, (void *) (long) i) == 0);
+  for (int i = 0; i < nthread; i++) {
+    assert(pthread_create(&tha[i], NULL, get_thread, (void *)(long)i) == 0);
   }
-  for(int i = 0; i < nthread; i++) {
+  for (int i = 0; i < nthread; i++) {
     assert(pthread_join(tha[i], &value) == 0);
   }
   t1 = now();
 
-  printf("%d gets, %.3f seconds, %.0f gets/second\n",
-         NKEYS*nthread, t1 - t0, (NKEYS*nthread) / (t1 - t0));
+  printf("%d gets, %.3f seconds, %.0f gets/second\n", NKEYS * nthread, t1 - t0,
+         (NKEYS * nthread) / (t1 - t0));
 }
