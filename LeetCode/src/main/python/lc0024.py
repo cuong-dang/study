87d364b5d354aeaf26d1a1bead301a4be9cc7d17
Cuong Dang <cuongd@pm.me>
2024-02-13 23:45:57 -0600

    lc/0020: Solve 0024

---
+++ LeetCode/src/main/python/lc0024.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        prev, curr = None, head
+        swapping = True
+        while curr.next is not None:
+            if swapping:
+                # Special case, first swap
+                if prev is None:
+                    t = curr.next
+                    curr.next = t.next
+                    t.next = curr
+                    head = t
+                    curr = t
+                else:
+                    t = curr.next
+                    curr.next = t.next
+                    t.next = curr
+                    prev.next = t
+                    curr = t
+            prev = curr
+            curr = curr.next
+            swapping = not swapping
+        return head
+
+
+if __name__ == '__main__':
+    Solution().swapPairs(ListNode(1, ListNode(2, ListNode(3, ListNode(4)))))
