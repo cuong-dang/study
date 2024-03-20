2ba15857ade865f287ea4928ce2b96cc2efc733b
Cuong Dang <cuongd@pm.me>
2024-03-20 14:38:54 -0500

    xv6/net: Init

--- xv6/kernel/file.c
+++ xv6/kernel/file.c
@@ -19,15 +19,21 @@ struct {
   struct file file[NFILE];
 } ftable;
 
-void fileinit(void) { initlock(&ftable.lock, "ftable"); }
+void
+fileinit(void)
+{
+  initlock(&ftable.lock, "ftable");
+}
 
 // Allocate a file structure.
-struct file *filealloc(void) {
+struct file*
+filealloc(void)
+{
   struct file *f;
 
   acquire(&ftable.lock);
-  for (f = ftable.file; f < ftable.file + NFILE; f++) {
-    if (f->ref == 0) {
+  for(f = ftable.file; f < ftable.file + NFILE; f++){
+    if(f->ref == 0){
       f->ref = 1;
       release(&ftable.lock);
       return f;
@@ -38,33 +44,27 @@ struct file *filealloc(void) {
 }
 
 // Increment ref count for file f.
-struct file *filedup(struct file *f) {
+struct file*
+filedup(struct file *f)
+{
   acquire(&ftable.lock);
-  if (f->ref < 1)
+  if(f->ref < 1)
     panic("filedup");
   f->ref++;
   release(&ftable.lock);
   return f;
 }
 
-// Decrement ref count for file f.
-struct file *filededup(struct file *f) {
-  acquire(&ftable.lock);
-  if (f->ref < 1)
-    panic("filededup");
-  f->ref--;
-  release(&ftable.lock);
-  return f;
-}
-
 // Close file f.  (Decrement ref count, close when reaches 0.)
-void fileclose(struct file *f) {
+void
+fileclose(struct file *f)
+{
   struct file ff;
 
   acquire(&ftable.lock);
-  if (f->ref < 1)
+  if(f->ref < 1)
     panic("fileclose");
-  if (--f->ref > 0) {
+  if(--f->ref > 0){
     release(&ftable.lock);
     return;
   }
@@ -73,26 +73,33 @@ void fileclose(struct file *f) {
   f->type = FD_NONE;
   release(&ftable.lock);
 
-  if (ff.type == FD_PIPE) {
+  if(ff.type == FD_PIPE){
     pipeclose(ff.pipe, ff.writable);
-  } else if (ff.type == FD_INODE || ff.type == FD_DEVICE) {
+  } else if(ff.type == FD_INODE || ff.type == FD_DEVICE){
     begin_op();
     iput(ff.ip);
     end_op();
   }
+#ifdef LAB_NET
+  else if(ff.type == FD_SOCK){
+    sockclose(ff.sock);
+  }
+#endif
 }
 
 // Get metadata about file f.
 // addr is a user virtual address, pointing to a struct stat.
-int filestat(struct file *f, uint64 addr) {
+int
+filestat(struct file *f, uint64 addr)
+{
   struct proc *p = myproc();
   struct stat st;
-
-  if (f->type == FD_INODE || f->type == FD_DEVICE) {
+  
+  if(f->type == FD_INODE || f->type == FD_DEVICE){
     ilock(f->ip);
     stati(f->ip, &st);
     iunlock(f->ip);
-    if (copyout(p->pagetable, addr, (char *)&st, sizeof(st)) < 0)
+    if(copyout(p->pagetable, addr, (char *)&st, sizeof(st)) < 0)
       return -1;
     return 0;
   }
@@ -101,24 +108,32 @@ int filestat(struct file *f, uint64 addr) {
 
 // Read from file f.
 // addr is a user virtual address.
-int fileread(struct file *f, uint64 addr, int n) {
+int
+fileread(struct file *f, uint64 addr, int n)
+{
   int r = 0;
 
-  if (f->readable == 0)
+  if(f->readable == 0)
     return -1;
 
-  if (f->type == FD_PIPE) {
+  if(f->type == FD_PIPE){
     r = piperead(f->pipe, addr, n);
-  } else if (f->type == FD_DEVICE) {
-    if (f->major < 0 || f->major >= NDEV || !devsw[f->major].read)
+  } else if(f->type == FD_DEVICE){
+    if(f->major < 0 || f->major >= NDEV || !devsw[f->major].read)
       return -1;
     r = devsw[f->major].read(1, addr, n);
-  } else if (f->type == FD_INODE) {
+  } else if(f->type == FD_INODE){
     ilock(f->ip);
-    if ((r = readi(f->ip, 1, addr, f->off, n)) > 0)
+    if((r = readi(f->ip, 1, addr, f->off, n)) > 0)
       f->off += r;
     iunlock(f->ip);
-  } else {
+  }
+#ifdef LAB_NET
+  else if(f->type == FD_SOCK){
+    r = sockread(f->sock, addr, n);
+  }
+#endif
+  else {
     panic("fileread");
   }
 
@@ -127,30 +142,32 @@ int fileread(struct file *f, uint64 addr, int n) {
 
 // Write to file f.
 // addr is a user virtual address.
-int filewrite(struct file *f, uint64 addr, int n) {
+int
+filewrite(struct file *f, uint64 addr, int n)
+{
   int r, ret = 0;
 
-  if (f->writable == 0)
+  if(f->writable == 0)
     return -1;
 
-  if (f->type == FD_PIPE) {
+  if(f->type == FD_PIPE){
     ret = pipewrite(f->pipe, addr, n);
-  } else if (f->type == FD_DEVICE) {
-    if (f->major < 0 || f->major >= NDEV || !devsw[f->major].write)
+  } else if(f->type == FD_DEVICE){
+    if(f->major < 0 || f->major >= NDEV || !devsw[f->major].write)
       return -1;
     ret = devsw[f->major].write(1, addr, n);
-  } else if (f->type == FD_INODE) {
+  } else if(f->type == FD_INODE){
     // write a few blocks at a time to avoid exceeding
     // the maximum log transaction size, including
     // i-node, indirect block, allocation blocks,
     // and 2 blocks of slop for non-aligned writes.
     // this really belongs lower down, since writei()
     // might be writing a device like the console.
-    int max = ((MAXOPBLOCKS - 1 - 1 - 2) / 2) * BSIZE;
+    int max = ((MAXOPBLOCKS-1-1-2) / 2) * BSIZE;
     int i = 0;
-    while (i < n) {
+    while(i < n){
       int n1 = n - i;
-      if (n1 > max)
+      if(n1 > max)
         n1 = max;
 
       begin_op();
@@ -160,16 +177,23 @@ int filewrite(struct file *f, uint64 addr, int n) {
       iunlock(f->ip);
       end_op();
 
-      if (r != n1) {
+      if(r != n1){
         // error from writei
         break;
       }
       i += r;
     }
     ret = (i == n ? n : -1);
-  } else {
+  }
+#ifdef LAB_NET
+  else if(f->type == FD_SOCK){
+    ret = sockwrite(f->sock, addr, n);
+  }
+#endif
+  else {
     panic("filewrite");
   }
 
   return ret;
 }
+
--- xv6/kernel/proc.h
+++ xv6/kernel/proc.h
@@ -20,10 +20,10 @@ struct context {
 
 // Per-CPU state.
 struct cpu {
-  struct proc *proc;      // The process running on this cpu, or null.
-  struct context context; // swtch() here to enter scheduler().
-  int noff;               // Depth of push_off() nesting.
-  int intena;             // Were interrupts enabled before push_off()?
+  struct proc *proc;          // The process running on this cpu, or null.
+  struct context context;     // swtch() here to enter scheduler().
+  int noff;                   // Depth of push_off() nesting.
+  int intena;                 // Were interrupts enabled before push_off()?
 };
 
 extern struct cpu cpus[NCPU];
@@ -82,29 +82,17 @@ struct trapframe {
 
 enum procstate { UNUSED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
-struct vma {
-  int isfree;
-  uint64 addr;
-  int len;
-  int prot;
-  int flags;
-  int fd;
-  struct file *f;
-  int offset;
-  int mapped_sz;
-};
-
 // Per-process state
 struct proc {
   struct spinlock lock;
 
   // p->lock must be held when using these:
-  enum procstate state; // Process state
-  struct proc *parent;  // Parent process
-  void *chan;           // If non-zero, sleeping on chan
-  int killed;           // If non-zero, have been killed
-  int xstate;           // Exit status to be returned to parent's wait
-  int pid;              // Process ID
+  enum procstate state;        // Process state
+  struct proc *parent;         // Parent process
+  void *chan;                  // If non-zero, sleeping on chan
+  int killed;                  // If non-zero, have been killed
+  int xstate;                  // Exit status to be returned to parent's wait
+  int pid;                     // Process ID
 
   // these are private to the process, so p->lock need not be held.
   uint64 kstack;               // Virtual address of kernel stack
@@ -115,7 +103,4 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-
-  // vma
-  struct vma vmas[16];
 };
