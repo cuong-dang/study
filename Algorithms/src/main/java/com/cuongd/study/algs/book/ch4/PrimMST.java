636ff8e34c66617df0c164a8aee0dab802d6548e
Cuong Dang <cuongd@pm.me>
2024-08-22 11:10:09 -0500

    algs/ch4.3: Complete exercise 4.3.26

--- Algorithms/src/main/java/com/cuongd/study/algs/book/ch4/PrimMST.java
+++ Algorithms/src/main/java/com/cuongd/study/algs/book/ch4/PrimMST.java
@@ -6,9 +6,14 @@ import edu.princeton.cs.algs4.IndexMinPQ;
 import java.util.Arrays;
 
 public class PrimMST {
+    private static final int NOT_MST_EDGE = -1;
+    private static final int NOT_CRITICAL = 0;
+    private static final int CRITICAL = 1;
+
     private Edge[] edgeTo;
     private double[] distTo;
     private boolean[] marked;
+    private int[] critical;
     private IndexMinPQ<Double> pq;
 
     public PrimMST(EdgeWeightedGraph G) {
@@ -16,6 +21,8 @@ public class PrimMST {
         distTo = new double[G.V()];
         Arrays.fill(distTo, Double.POSITIVE_INFINITY);
         marked = new boolean[G.V()];
+        critical = new int[G.V()];
+        Arrays.fill(critical, NOT_MST_EDGE);
         pq = new IndexMinPQ<>(G.V());
 
         distTo[0] = 0.0;
@@ -35,11 +42,14 @@ public class PrimMST {
             if (e.weight() < distTo[w]) {
                 edgeTo[w] = e;
                 distTo[w] = e.weight();
+                critical[w] = CRITICAL;
                 if (pq.contains(w)) {
                     pq.changeKey(w, e.weight());
                 } else {
                     pq.insert(w, e.weight());
                 }
+            } else if (e.weight() == distTo[w]) {
+                critical[w] = NOT_CRITICAL;
             }
         }
     }
@@ -50,5 +60,14 @@ public class PrimMST {
             mst.add(edgeTo[i]);
         }
         return mst;
+    }
+
+    public boolean isCritical(Edge e) {
+        for (int v = 0; v < edgeTo.length; v++) {
+            if (edgeTo[v] == e) {
+                return critical[v] == CRITICAL;
+            }
+        }
+        return false;
     }
 }
