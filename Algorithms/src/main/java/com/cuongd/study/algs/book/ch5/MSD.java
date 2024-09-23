819d9be9c97ecfbb934f45154bb92bee6a10b818
Cuong Dang <cuongd@pm.me>
2024-09-23 16:40:22 -0500

    algs/ch5.1: Add Quick3string

--- Algorithms/src/main/java/com/cuongd/study/algs/book/ch5/MSD.java
+++ Algorithms/src/main/java/com/cuongd/study/algs/book/ch5/MSD.java
@@ -5,9 +5,9 @@ import edu.princeton.cs.algs4.Insertion;
 import java.util.Comparator;
 
 public class MSD {
-    private static int R = 256;
-    private static int M = 15;
-    private static String[] aux;
+    private static int R = 256; // radix
+    private static int M = 15; // cutoff for small arrays
+    private static String[] aux; // auxiliary array for distribution
 
     private static int charAt(String s, int d) {
         return d < s.length() ? s.charAt(d) : -1;
@@ -26,16 +26,16 @@ public class MSD {
             return;
         }
         int[] count = new int[R+2];
-        for (int i = lo; i <= hi; i++) {
+        for (int i = lo; i <= hi; i++) { // Compute frequency counts.
             count[charAt(a[i], d) + 2]++;
         }
-        for (int r = 0; r < R+1; r++) {
+        for (int r = 0; r < R+1; r++) { // Transform counts to indices.
             count[r+1] += count[r];
         }
-        for (int i = lo; i <= hi; i++) {
+        for (int i = lo; i <= hi; i++) { // Distribute.
             aux[count[charAt(a[i], d) + 1]++] = a[i];
         }
-        for (int i = lo; i <= hi; i++) {
+        for (int i = lo; i <= hi; i++) { // Copy back.
             a[i] = aux[i - lo];
         }
         for (int r = 0; r < R; r++) {
