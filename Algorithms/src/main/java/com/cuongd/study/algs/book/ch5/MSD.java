a26f3dc543e3c3f801bb6b9386be4db7cc0a3eca
Cuong Dang <cuongd@pm.me>
2024-09-25 14:59:44 -0500

    algs/ch5.1: Complete exercise 5.1.9

--- Algorithms/src/main/java/com/cuongd/study/algs/book/ch5/MSD.java
+++ Algorithms/src/main/java/com/cuongd/study/algs/book/ch5/MSD.java
@@ -1,9 +1,5 @@
 package com.cuongd.study.algs.book.ch5;
 
-import edu.princeton.cs.algs4.Insertion;
-
-import java.util.Comparator;
-
 public class MSD {
     private static int R = 256; // radix
     private static int M = 15; // cutoff for small arrays
@@ -20,11 +16,7 @@ public class MSD {
     }
 
     private static void sort(String[] a, int lo, int hi, int d) {
-        if (hi <= lo + M) {
-            Insertion.sort(a, lo, hi,
-                    Comparator.comparingInt(s -> charAt((String) s, d)));
-            return;
-        }
+        if (hi <= lo) return;
         int[] count = new int[R+2];
         for (int i = lo; i <= hi; i++) { // Compute frequency counts.
             count[charAt(a[i], d) + 2]++;
