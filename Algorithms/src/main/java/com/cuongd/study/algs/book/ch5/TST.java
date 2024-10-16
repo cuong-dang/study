ba966bf65920751cdf6ed51fff383abdab9c5a1e
Cuong Dang <cuongd@pm.me>
2024-10-16 16:36:52 -0500

    algs/ch5.2: Imple TST.keys

--- Algorithms/src/main/java/com/cuongd/study/algs/book/ch5/TST.java
+++ Algorithms/src/main/java/com/cuongd/study/algs/book/ch5/TST.java
@@ -1,5 +1,7 @@
 package com.cuongd.study.algs.book.ch5;
 
+import edu.princeton.cs.algs4.Queue;
+
 public class TST<Value> {
     private class Node {
         char c;
@@ -31,10 +33,40 @@ public class TST<Value> {
     private Node put(Node x, String key, Value val, int d) {
         char c = key.charAt(d);
         if (x == null) { x = new Node(); x.c = c; }
-        if (x.c < c) x.left = put(x.left, key, val, d);
-        else if (x.c > c) x.right = put(x.right, key, val, d);
+        if (c < x.c) x.left = put(x.left, key, val, d);
+        else if (c > x.c) x.right = put(x.right, key, val, d);
         else if (d < key.length() - 1) x.mid = put(x.mid, key, val, d+1);
         else x.val = val;
         return x;
+    }
+
+    public Iterable<String> keys() {
+        Queue<String> q = new Queue<>();
+        collect(root, "", q);
+        return q;
+    }
+
+    private void collect(Node x, String key, Queue<String> q) {
+        if (x == null) return;
+        if (x.val != null) {
+            q.enqueue(key + x.c);
+        }
+        collect(x.left, key, q);
+        collect(x.mid, key + x.c, q);
+        collect(x.right, key, q);
+    }
+
+    public static void main(String[] args) {
+        TST<Integer> t = new TST<>();
+        t.put("shells", 15);
+        t.put("by", 4);
+        t.put("the", 8);
+        t.put("are", 12);
+        t.put("sells", 11);
+        t.put("sea", 14);
+        t.put("surely", 13);
+        t.put("she", 10);
+        t.put("shore", 7);
+        t.keys().forEach(System.out::println);
     }
 }
