dd805ec03c3969513e849ab91e2b83c34a276acb
Cuong Dang <cuongd@pm.me>
2024-12-11 23:37:52 -0600

    dragon/ch2: Complete exercise 2.6.2

--- dragon/src/main/java/com/cuongd/study/dragon/ch2/lexer/Lexer.java
+++ dragon/src/main/java/com/cuongd/study/dragon/ch2/lexer/Lexer.java
@@ -1,7 +1,7 @@
 package com.cuongd.study.dragon.ch2.lexer;
 
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.util.Hashtable;
 
 public class Lexer {
     public int line = 1;
@@ -14,69 +14,105 @@ public class Lexer {
     }
 
     public Lexer() {
-        reserve( new Word(Tag.TRUE, "true") );
-        reserve( new Word(Tag.FALSE, "false") );
+        reserve(new Word(Tag.TRUE, "true"));
+        reserve(new Word(Tag.FALSE, "false"));
     }
 
     public Token scan() throws IOException {
         boolean isLineComment = false, isBlockComment = false;
-        for( ; ; peek = read() ) {
+        for (; ; peek = read()) {
             // end of line comment?
-            if( peek == '\n' ) {
+            if (peek == '\n') {
                 line = line + 1;
-                if( isLineComment )
+                if (isLineComment)
                     isLineComment = false;
-            // are we in line comment?
-            } else if ( isLineComment ) continue;
-            // end of block comment?
-            else if ( isBlockComment && peek == '*' ) {
+                // are we in line comment?
+            } else if (isLineComment) continue;
+                // end of block comment?
+            else if (isBlockComment && peek == '*') {
                 peek = read();
                 if (peek == '/')
                     isBlockComment = false;
                 else
                     putBack = peek;
-            // are we in block comment?
-            } else if ( isBlockComment ) continue;
-            // not in comments
-            else if ( peek == '/' ) {
+                // are we in block comment?
+            } else if (isBlockComment) continue;
+                // not in comments
+            else if (peek == '/') {
                 peek = read();
-                if ( peek == '/' )
+                if (peek == '/')
                     isLineComment = true;
-                else if ( peek == '*' )
+                else if (peek == '*')
                     isBlockComment = true;
                 else {
                     putBack = peek;
                     peek = '/';
                     break;
                 }
-            // skip white spaces
-            } else if( peek == ' ' || peek == '\t' ) continue;
+                // skip white spaces
+            } else if (peek == ' ' || peek == '\t') continue;
             else break;
         }
-        if( Character.isDigit(peek) ) {
+        if (Character.isDigit(peek)) {
             int v = 0;
             do {
-                v = 10*v + Character.digit(peek, 10);
+                v = 10 * v + Character.digit(peek, 10);
                 peek = read();
-            } while ( Character.isDigit(peek) );
+            } while (Character.isDigit(peek));
             return new Num(v);
         }
-        if( Character.isLetter(peek) ) {
+        if (Character.isLetter(peek)) {
             StringBuffer b = new StringBuffer();
             do {
                 b.append(peek);
                 peek = read();
-            } while ( Character.isLetterOrDigit(peek) );
+            } while (Character.isLetterOrDigit(peek));
             String s = b.toString();
-            Word w = (Word)words.get(s);
-            if( w != null ) return w;
+            Word w = (Word) words.get(s);
+            if (w != null) return w;
             w = new Word(Tag.ID, s);
             words.put(s, w);
             return w;
         }
-        Token t = new Token(peek);
+        Token t;
+        switch (peek) {
+            case '<':
+                t = cmpOpCheckNext(CmpOp.LTEQ, CmpOp.LT);
+                break;
+            case '>':
+                t = cmpOpCheckNext(CmpOp.GTEQ, CmpOp.GT);
+                break;
+            case '=':
+                t = cmpOpCheckNext(CmpOp.EQ);
+                break;
+            case '!':
+                t = cmpOpCheckNext(CmpOp.NEQ);
+                break;
+            default:
+                t = new Token(peek);
+        }
         peek = ' ';
         return t;
+    }
+
+    private Token cmpOpCheckNext(int matched, int notMatched)
+            throws IOException {
+        char next = read();
+        if (next == '=') {
+            return new CmpOp(matched);
+        }
+        putBack = next;
+        return new CmpOp(notMatched);
+    }
+
+    private Token cmpOpCheckNext(int matched)
+            throws IOException {
+        char next = read();
+        if (next == '=') {
+            return new CmpOp(matched);
+        }
+        putBack = next;
+        return new Token(peek);
     }
 
     private char read() throws IOException {
@@ -85,7 +121,7 @@ public class Lexer {
             putBack = ' ';
             return t;
         }
-        return (char)System.in.read();
+        return (char) System.in.read();
     }
 
     public static void main(String[] args) throws IOException {
--- dragon/src/main/java/com/cuongd/study/dragon/ch2/lexer/Tag.java
+++ dragon/src/main/java/com/cuongd/study/dragon/ch2/lexer/Tag.java
@@ -2,5 +2,8 @@ package com.cuongd.study.dragon.ch2.lexer;
 
 public class Tag {
     public final static int
-        NUM = 256, ID = 257, TRUE = 258, FALSE = 259;
+            NUM = 256,
+            ID = 257,
+            TRUE = 258, FALSE = 259,
+            CMP_OP = 260;
 }
