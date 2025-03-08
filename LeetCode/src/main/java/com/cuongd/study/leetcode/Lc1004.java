31406d6d2fc5eb911ef69d9838ceab76aa03ddce
Cuong Dang <cuongd@pm.me>
2025-03-07 20:43:14 -0600

    lc/fb2: Fast 1004

--- LeetCode/src/main/java/com/cuongd/study/leetcode/Lc1004.java
+++ LeetCode/src/main/java/com/cuongd/study/leetcode/Lc1004.java
@@ -1,30 +1,21 @@
 package com.cuongd.study.leetcode;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 public class Lc1004 {
     public int longestOnes(int[] nums, int k) {
-        int start = 0, max = 0;
-        Queue<Integer> flipped = new ArrayDeque<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 1) continue;
-            if (flipped.size() < k) {
-                flipped.add(i);
-                continue;
-            }
-            if (i - start > max) {
-                max = i - start;
-            }
-            start = flipped.isEmpty() ? i + 1 : flipped.remove() + 1;
-            if (flipped.size() < k) {
-                flipped.add(i);
+        int i = 0, j = 0, ans = 0;
+        while (j < nums.length) {
+            if (nums[j] == 1 || k > 0) {
+                k -= 1 - nums[j];
+                j++;
+            } else {
+                ans = Math.max(ans, j - i);
+                while (k == 0) {
+                    k += 1 - nums[i++];
+                }
             }
         }
-        if (nums.length - start > max) {
-            max = nums.length - start;
-        }
-        return max;
+        ans = Math.max(ans, nums.length - i);
+        return ans;
     }
 
     public static void main(String[] args) {
