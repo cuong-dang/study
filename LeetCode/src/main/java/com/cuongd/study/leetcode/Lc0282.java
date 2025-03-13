ef2dc4a708280b6c9db250fd41e6077e8aeeff6c
Cuong Dang <cuongd@pm.me>
2025-03-13 14:48:16 -0500

    lc/fb2: Solve 0282

--- LeetCode/src/main/java/com/cuongd/study/leetcode/Lc0282.java
+++ LeetCode/src/main/java/com/cuongd/study/leetcode/Lc0282.java
@@ -4,57 +4,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lc0282 {
+    private String n;
     private int target;
     private List<String> ans;
 
     public List<String> addOperators(String num, int target) {
+        this.n = num;
         this.target = target;
         ans = new ArrayList<>();
-        run(num, 0, 0, 0, 0, new StringBuilder());
+        run(0, 0, 0, 0, new ArrayList<>());
         return ans;
     }
 
-    private void run(String s, int i, long currVal, long undo, long t, StringBuilder sb) {
-        if (i == s.length()) {
-            if (currVal == target) {
+    private void run(int i, long value, long undo, long operand, List<String> collector) {
+        if (i == n.length()) {
+            if (value == target && operand == 0) {
+                StringBuilder sb = new StringBuilder();
+                collector.subList(1, collector.size()).forEach(sb::append);
                 ans.add(sb.toString());
             }
             return;
         }
-        long val = t * 10 + Character.getNumericValue(s.charAt(i));
-        String valS = Long.toString(val);
-        if (i == 0) {
-            sb.append(s.charAt(0));
-            run(s, 1, val, val, 0, sb);
-            return;
+        operand = operand * 10 + Character.getNumericValue(n.charAt(i));
+        String opStr = Long.toString(operand);
+        if (operand > 0) {
+            run(i + 1, value, undo, operand, collector);
         }
-//        if (val > 0) {
-//            run(s, i + 1, currVal, undo, val, sb);
-//        }
         // try add
-        sb.append("+");
-        sb.append(valS);
-        run(s, i + 1, currVal + val, val, 0, sb);
-        sb.deleteCharAt(sb.length() - 1);
-        sb.deleteCharAt(sb.length() - 1);
-        // try minus
-        sb.append("-");
-        sb.append(valS);
-        run(s, i + 1, currVal - val, -val, 0, sb);
-        sb.deleteCharAt(sb.length() - 1);
-        sb.deleteCharAt(sb.length() - 1);
-        // try multiply
-        sb.append("*");
-        sb.append(valS);
-        run(s, i + 1, (currVal - undo) + undo * val, undo * val, 0, sb);
-        sb.deleteCharAt(sb.length() - 1);
-        sb.deleteCharAt(sb.length() - 1);
+        collector.add("+");
+        collector.add(opStr);
+        run(i + 1, value + operand, operand, 0, collector);
+        collector.remove(collector.size() - 1);
+        collector.remove(collector.size() - 1);
+        if (!collector.isEmpty()) {
+            // try minus
+            collector.add("-");
+            collector.add(opStr);
+            run(i + 1, value - operand, -operand, 0, collector);
+            collector.remove(collector.size() - 1);
+            collector.remove(collector.size() - 1);
+            // try multiply
+            collector.add("*");
+            collector.add(opStr);
+            run(i + 1, (value - undo) + undo * operand, undo * operand, 0, collector);
+            collector.remove(collector.size() - 1);
+            collector.remove(collector.size() - 1);
+        }
     }
 
     public static void main(String[] args) {
         System.out.println(new Lc0282().addOperators("123", 6));
         System.out.println(new Lc0282().addOperators("232", 8));
         System.out.println(new Lc0282().addOperators("105", 5));
-        System.out.println(new Lc0282().addOperators("3456237490", 9191));
+        System.out.println(new Lc0282().addOperators("123456789", 45));
     }
 }
