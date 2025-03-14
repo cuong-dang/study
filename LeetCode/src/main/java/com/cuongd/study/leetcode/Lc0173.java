ee2d01cbf126104307717626576e1ac89db01c26
Cuong Dang <cuongd@pm.me>
2025-03-13 22:05:38 -0500

    lc/fb2: Solve 0173

---
+++ LeetCode/src/main/java/com/cuongd/study/leetcode/Lc0173.java
@@ -0,0 +1,49 @@
+package com.cuongd.study.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Lc0173 {
+    class BSTIterator {
+        private Queue<Integer> q;
+
+        public BSTIterator(TreeNode root) {
+            q = new ArrayDeque<>();
+            walk(root);
+        }
+
+        private void walk(TreeNode x) {
+            if (x == null) return;
+            walk(x.left);
+            q.add(x.val);
+            walk(x.right);
+        }
+
+        public int next() {
+            return q.remove();
+        }
+
+        public boolean hasNext() {
+            return !q.isEmpty();
+        }
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
