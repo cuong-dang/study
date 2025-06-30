852ff117b4d5b12787a8805e98ecbe99a3b32156
Cuong Dang <cuongd@pm.me>
2025-06-30 16:36:52 -0500

    lc/appl: Solve 0570

---
+++ LeetCode/src/main/sql/Lc0570.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+WITH dr_count AS (
+    SELECT t1.id, t1.name, count(*) AS dr_count
+    FROM Employee t1 JOIN Employee t2 ON t1.id = t2.managerId
+    GROUP BY t1.id, t1.name
+)
+SELECT name
+FROM dr_count
+WHERE dr_count >= 5;
