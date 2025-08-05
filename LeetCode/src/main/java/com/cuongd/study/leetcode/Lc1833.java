5b7bc8e96a88eab062fd2bcbc62ae3d26e717419
Cuong Dang <cuongd@pm.me>
2025-08-04 23:02:32 -0500

    lc/appl: Counting sort 1833

--- LeetCode/src/main/java/com/cuongd/study/leetcode/Lc1833.java
+++ LeetCode/src/main/java/com/cuongd/study/leetcode/Lc1833.java
@@ -1,15 +1,35 @@
 package com.cuongd.study.leetcode;
 
-import java.util.Arrays;
-
 public class Lc1833 {
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
+        sort(costs);
         int res = 0;
         for (int i = 0; i < costs.length && costs[i] <= coins; i++) {
             res++;
             coins -= costs[i];
         }
         return res;
+    }
+
+    private void sort(int[] a) {
+        int max = a[0];
+        for (int e : a) {
+            if (e > max) {
+                max = e;
+            }
+        }
+
+        int[] count = new int[max + 2];
+        for (int e : a) {
+            count[e + 1]++;
+        }
+        for (int i = 0; i < count.length - 1; i++) {
+            count[i + 1] += count[i];
+        }
+        int[] aux = new int[a.length];
+        for (int j : a) {
+            aux[count[j]++] = j;
+        }
+        System.arraycopy(aux, 0, a, 0, a.length);
     }
 }
