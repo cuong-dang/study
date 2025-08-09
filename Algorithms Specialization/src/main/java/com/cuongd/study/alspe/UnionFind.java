207db88becede96a9ad5fa926c0efc10d97a31fb
Cuong Dang <cuongd@pm.me>
2025-08-09 10:13:30 -0500

    alspe/c3: Complete PA2.1

--- Algorithms Specialization/src/main/java/com/cuongd/study/alspe/UnionFind.java
+++ Algorithms Specialization/src/main/java/com/cuongd/study/alspe/UnionFind.java
@@ -2,14 +2,14 @@ package com.cuongd.study.alspe;
 
 public class UnionFind {
     private final int[] parent;
-    private final int[] size;
+    private final int[] rank;
 
     public UnionFind(int n) {
         parent = new int[n];
-        size = new int[n];
+        rank = new int[n];
         for (int i = 0; i < n; i++) {
             parent[i] = i;
-            size[i] = 1;
+            rank[i] = 0;
         }
     }
 
@@ -17,10 +17,13 @@ public class UnionFind {
         int p1 = find(i);
         int p2 = find(j);
         if (p1 == p2) return;
-        int demoted = size[p1] < size[p2] ? p1 : p2;
+        int demoted = rank[p1] < rank[p2] ? p1 : p2;
         int promoted = demoted == p1 ? p2 : p1;
+        boolean increasingRank = rank[p1] == rank[p2];
         parent[demoted] = promoted;
-        size[promoted] += size[demoted];
+        if (increasingRank) {
+            rank[promoted]++;
+        }
     }
 
     public int find(int i) {
@@ -35,32 +38,32 @@ public class UnionFind {
         for (int i = 0; i < 6; i++) {
             assert uf.find(i) == i;
             assert uf.parent[i] == i;
-            assert uf.size[i] == 1;
+            assert uf.rank[i] == 0;
         }
 
         uf.union(0, 1);
         assert uf.find(0) == uf.find(1);
         assert uf.parent[0] == 0;
         assert uf.parent[1] == 0;
-        assert uf.size[0] == 2;
-        assert uf.size[1] == 1;
+        assert uf.rank[0] == 1;
+        assert uf.rank[1] == 0;
 
         uf.union(2, 1);
         assert uf.parent[0] == 0;
         assert uf.parent[1] == 0;
         assert uf.parent[2] == 0;
-        assert uf.size[0] == 3;
-        assert uf.size[1] == 1;
-        assert uf.size[2] == 1;
+        assert uf.rank[0] == 1;
+        assert uf.rank[1] == 0;
+        assert uf.rank[2] == 0;
 
         uf.union(3, 4);
         uf.union(3, 5);
         assert uf.parent[3] == 3;
         assert uf.parent[4] == 3;
         assert uf.parent[5] == 3;
-        assert uf.size[3] == 3;
-        assert uf.size[4] == 1;
-        assert uf.size[5] == 1;
+        assert uf.rank[3] == 1;
+        assert uf.rank[4] == 0;
+        assert uf.rank[5] == 0;
 
         uf.union(2, 5);
         assert uf.parent[0] == 0;
@@ -69,11 +72,11 @@ public class UnionFind {
         assert uf.parent[3] == 0;
         assert uf.parent[4] == 3;
         assert uf.parent[5] == 3;
-        assert uf.size[0] == 6;
-        assert uf.size[1] == 1;
-        assert uf.size[2] == 1;
-        assert uf.size[3] == 3;
-        assert uf.size[4] == 1;
-        assert uf.size[5] == 1;
+        assert uf.rank[0] == 2;
+        assert uf.rank[1] == 0;
+        assert uf.rank[2] == 0;
+        assert uf.rank[3] == 1;
+        assert uf.rank[4] == 0;
+        assert uf.rank[5] == 0;
     }
 }
