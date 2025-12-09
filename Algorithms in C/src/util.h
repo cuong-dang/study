3f6737894958739547231de24919fb0924c336dd
Cuong Dang <cuongd@pm.me>
2025-12-09 10:24:31 -0600

    aic/ch1: Exercise 1.20

--- Algorithms in C/src/util.h
+++ Algorithms in C/src/util.h
@@ -1,6 +1,33 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-int randint(int min, int max);
+#include <stdlib.h>
+#include <time.h>
+
+int randint(int min, int max) { return min + rand() % (max - min + 1); }
+
+double timeit(void (*fp)(void)) {
+  struct timespec start, end;
+  double elapsed;
+
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  (*fp)();
+  clock_gettime(CLOCK_MONOTONIC, &end);
+
+  elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+  return elapsed;
+}
+
+double timeit_n(void (*fp)(void), int n) {
+  double elapsed;
+  int i;
+
+  elapsed = 0;
+  for (i = 0; i < n; i++) {
+    elapsed += timeit(fp);
+  }
+  return elapsed / n;
+  ;
+}
 
 #endif // UTIL_H
